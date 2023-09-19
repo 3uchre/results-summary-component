2041ebbb87e2e5f459f2d3b00199268e973e9bb0
3uchre <98996113+3uchre@users.noreply.github.com>
2023-09-19 01:38:46 +0100

    another attempt to print json data

--- data.js
+++ data.js
@@ -1,11 +1,8 @@
 fetch('./data.json')
-    .then(response => response.json())
-    .then(data => {
-        
-        var table = document.getElementById("table");
-        for (var i = 0, cell; cell = table.cells[i]; i++) {
-            document.getElementByClassName("icon")[i].src = data[i].icon;
-            document.getElementByClassName("category")[i].innerHTML = data[i].category;
-            document.getElementByClassName("score")[i].innerHTML = data[i].score;
-        }
+.then(response => response.json())
+.then(data => {
+    document.getElementsByClassName("icon")[0].src = data[0].icon;
+    document.getElementsByClassName("category")[0].innerHTML = data[0].category;
+    document.getElementsByClassName("score")[0].innerHTML = data[0].score;
 })
+.catch(error => console.error('Error:', error));
