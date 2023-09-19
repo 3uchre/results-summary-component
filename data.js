3c95a46c85888398edf4e8a4b580232a1fc3d106
3uchre <98996113+3uchre@users.noreply.github.com>
2023-09-19 01:41:18 +0100

    another attempt

--- data.js
+++ data.js
@@ -1,8 +1,6 @@
 fetch('./data.json')
 .then(response => response.json())
 .then(data => {
-    document.getElementsByClassName("icon")[0].src = data[0].icon;
-    document.getElementsByClassName("category")[0].innerHTML = data[0].category;
-    document.getElementsByClassName("score")[0].innerHTML = data[0].score;
+    document.getElementByID("description")[1].innerHTML = "hello please";
 })
 .catch(error => console.error('Error:', error));
