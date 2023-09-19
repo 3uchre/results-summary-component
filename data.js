4220f923c3495bcf4adc949f06b70d69a4df379e
3uchre <98996113+3uchre@users.noreply.github.com>
2023-09-19 03:11:59 +0100

    script populates table from json + displays avg

--- data.js
+++ data.js
@@ -1,6 +1,19 @@
 fetch('./data.json')
-.then(response => response.json())
-.then(data => {
-    document.getElementByID("description")[1].innerHTML = "hello please";
-})
-.catch(error => console.error('Error:', error));
+    .then(response => response.json())
+    .then(data => {
+        var table = document.getElementById("table");
+        for (var r = 0; r < table.rows.length; r++) {
+            document.getElementsByClassName("icon")[r].src = data[r].icon;
+            document.getElementsByClassName("category")[r].textContent = data[r].category;
+            document.getElementsByClassName("score")[r].prepend(data[r].score);
+        }
+        var total = 0;
+        for (var i = 0; i < data.length; i++) {
+            total += data[i].score;
+        }
+        const average = Math.round(total / data.length);
+        document.getElementById("average").textContent = average;
+        
+
+    })
+    .catch(error => console.error('Error:', error));
